093be02fcaffa9aa1d616b8f580b38febeccc947
fabien pittiani <fabien.pittiani@gmail.com>
2024-04-29 19:25:50 +0200

    section portfolios ok

--- src/components/CardPortfolio/CardPortfolio.js
+++ src/components/CardPortfolio/CardPortfolio.js
@@ -6,13 +6,13 @@ const CardPortfolio = ({ imageUrl, description, name, url, techno }) => {
     <div className={styles.card}>
       <img src={imageUrl} alt={`logo ${name}`} />
 
-      <div className={styles.card - content}>
+      <div className={styles['card-content']}>
         <p>{description}</p>
         <a href={url}>{`Visiter : ${name}`}</a>
-        <div className="technologies">
+        <div className={styles.technologies}>
           {techno && techno.length
             ? techno.map((itemTechno) => (
-                <div className="techno-item">{itemTechno}</div>
+                <span className={styles['techno-item']}>{itemTechno}</span>
               ))
             : null}
         </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,12 +2,33 @@ import React from "react";
 import Title from "../Title/Title";
 import Card from "../CardPortfolio/CardPortfolio";
 import styles from "./Portfolio.module.css";
+import ProduitBrut from "../../assets/produitbrut.png"
 
 const Portfolio = () => {
   return (
     <div className={styles.portfolio}>
       <div className={styles.container}>
         <Title name={"Mon Portfolio"} />
+        <div className={styles.cards}>
+
+          <Card imageUrl={ProduitBrut}
+           description= "Site de communication pour un maitre d’ouvrage, avec personnalisation de sa gallerie."
+           name= "produitbrut.fr"
+           url="https://www.produitbrut.fr"
+           techno ={["Rails", "Javascript", "Html/Css"]}/>
+
+<Card imageUrl={ProduitBrut}
+           description= "Site de communication pour un maitre d’ouvrage, avec personnalisation de sa gallerie."
+           name= "produitbrut.fr"
+           url="https://www.produitbrut.fr"
+           techno ={["Rails", "Javascript", "Html/Css"]}/>
+
+<Card imageUrl={ProduitBrut}
+           description= "Site de communication pour un maitre d’ouvrage, avec personnalisation de sa gallerie."
+           name= "produitbrut.fr"
+           url="https://www.produitbrut.fr"
+           techno ={["Rails", "Javascript", "Html/Css"]}/>
+        </div>
       </div>
     </div>
   );
