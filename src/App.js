8cf36b392e891f483162ad8b97b4aff68e799d4b
fabien pittiani <fabien.pittiani@gmail.com>
2024-05-03 09:40:16 +0200

    ajout analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Index from "./components/index";
 import "./App.css";
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-SRMQ6H9L1H"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
